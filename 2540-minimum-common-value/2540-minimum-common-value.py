1a899e2a1232342b235aedbfcb6c42f1f47e3766
Sidhujha <137885707+Sidhujha@users.noreply.github.com>
2024-03-31 00:11:41 +0530

    Time: 359 ms (22.45%), Space: 42.7 MB (15.41%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -1,9 +1,4 @@
-class Solution(object):
-    def getCommon(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: int
-        """
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
         inter=set(nums1).intersection(set(nums2))
         return min(inter) if len(inter)>0  else -1 
