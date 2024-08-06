32e35274f99a7c92426e9f0e3de74b59682664b7
Sidhujha <137885707+Sidhujha@users.noreply.github.com>
2024-08-06 17:19:08 +0530

    Time: 7287 ms (5.01%), Space: 70.6 MB (8.77%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,19 +1,23 @@
 class Solution:
+    def perm(self,ind,li,res,k):
+        if len(res)==k+35000:
+            return
+        if ind==len(li):
+            res.append(list(li))
+            return
+        for i in range(ind,len(li)):
+            li[i],li[ind]=li[ind],li[i]
+            self.perm(ind+1,li,res,k)
+            li[i],li[ind]=li[ind],li[i]
     def getPermutation(self, n: int, k: int) -> str:
-        fac=1
         li=[]
-        for i in range(1,n):
-            fac*=i
+        res=[]
+        for i in range(1,n+1):
             li.append(i)
-        li.append(n)
-        k=k-1
+        self.perm(0,li,res,k)
+        res.sort()
         ans=""
-        while True:
-            a=li[k//fac]
-            ans=ans+str(a)
-            li.remove(a)
-            if len(li)==0:
-                break
-            k=k%fac
-            fac=fac//len(li)
+        for i in res[k-1]:
+            ans+=str(i)
         return ans
+        
