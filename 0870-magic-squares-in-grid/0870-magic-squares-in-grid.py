607a7343aaf3aa23a9d17c8e2c8d11ff621b2f55
Sidhujha <137885707+Sidhujha@users.noreply.github.com>
2024-08-10 01:19:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0870-magic-squares-in-grid/0870-magic-squares-in-grid.py
@@ -0,0 +1,31 @@
+class Solution:
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        c=0
+        li=[]
+        li=set(li)
+        for i in range(len(grid)-2):
+            for j in range(len(grid[0])-2):
+                if grid[i][j]>0 and grid[i][j]<10:
+                    li.add(grid[i][j])
+                if grid[i][j+1]>0 and grid[i][j+1]<10:
+                    li.add(grid[i][j+1])
+                if grid[i][j+2]>0 and grid[i][j+2]<10:
+                    li.add(grid[i][j+2])
+                if grid[i+1][j]>0 and grid[i+1][j]<10:
+                    li.add(grid[i+1][j])
+                if grid[i+1][j+1]>0 and grid[i+1][j+1]<10:
+                    li.add(grid[i+1][j+1])
+                if grid[i+1][j+2]>0 and grid[i+1][j+2]<10:
+                    li.add(grid[i+1][j+2])
+                if grid[i+2][j]>0 and grid[i+2][j]<10:
+                    li.add(grid[i+2][j])
+                if grid[i+2][j+1]>0 and grid[i+2][j+1]<10:
+                    li.add(grid[i+2][j+1])
+                if grid[i+2][j+2]>0 and grid[i+2][j+2]<10:
+                    li.add(grid[i+2][j+2])
+                if len(li)==9:
+                    if grid[i][j]+grid[i][j+1]+grid[i][j+2]==grid[i][j]+grid[i+1][j]+grid[i+2][j]==grid[i][j+1]+grid[i+1][j+1]+grid[i+2][j+1]==grid[i+1][j]+grid[i+1][j+1]+grid[i+1][j+2]==grid[i][j+2]+grid[i+1][j+2]+grid[i+2][j+2]==grid[i+2][j]+grid[i+2][j+1]+grid[i+2][j+2]==grid[i][j]+grid[i+1][j+1]+grid[i+2][j+2]==grid[i][j+2]+grid[i+1][j+1]+grid[i+2][j]:
+                        c+=1
+                else:
+                    li=set([])
+        return c
