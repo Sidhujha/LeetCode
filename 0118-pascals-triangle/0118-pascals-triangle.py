ba6f260d59370b8a5474e7c3e6557bc3d953cd0c
Sidhujha <137885707+Sidhujha@users.noreply.github.com>
2024-08-13 17:43:37 +0530

    Time: 34 ms (75.43%), Space: 16.6 MB (28.76%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,16 +1,14 @@
 class Solution:
-    def pas(self,r,c):
-        res=1
-        for i in range(c):
-            res=res*(r-i)
-            res=res//(i+1)
-        return res
     def generate(self, numRows: int) -> List[List[int]]:
         li=[]
         for i in range(1,numRows+1):
+            ans=1
             lis=[]
-            for j in range(1,i+1):
-                lis.append(self.pas(i-1,j-1))
+            lis.append(1)
+            for j in range(1,i):
+                ans=ans*(i-j)
+                ans=ans//j
+                lis.append(ans)
             li.append(lis)
         return li
         
