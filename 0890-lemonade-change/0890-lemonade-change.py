9db2822f2ede26ba6c1de209b6968dff51b321ee
Sidhujha <137885707+Sidhujha@users.noreply.github.com>
2024-08-15 15:57:55 +0530

    Time: 625 ms (38.2%), Space: 21.2 MB (58.06%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.py
+++ 0890-lemonade-change/0890-lemonade-change.py
@@ -1,27 +1,24 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        li=[]
+        c5=0
+        c10=0
         res=True
         for i in bills:
             if i==5:
-                li.append(i)
+                c5+=1
             if i==10:
-                if 5 in li:
-                    li.append(i)
-                    li.remove(5)
+                if c5>0:
+                    c10+=1
+                    c5-=1
                 else:
                     res=False
                     break
             if i==20:
-                if 5 in li and 10 in li:
-                    li.append(i)
-                    li.remove(5)
-                    li.remove(10)
-                elif 5 in li and li.count(5)>2:
-                    li.append(i)
-                    li.remove(5)
-                    li.remove(5)
-                    li.remove(5)
+                if c5>0 and c10>0:
+                    c5-=1
+                    c10-=1
+                elif c5>2:
+                    c5-=3
                 else:
                     res=False
                     break
