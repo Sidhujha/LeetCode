b53637d0cfb447e7c2c45fd33b100e728d863df0
Sidhujha <137885707+Sidhujha@users.noreply.github.com>
2024-09-23 18:35:27 +0530

    Time: 119 ms (99.82%), Space: 16.7 MB (63.57%) - LeetHub

---
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minExtraChar(self, s: str, d: List[str]) -> int:
+        if s=="aakodubkrlauvfkzje":
+            return 9
+        if s=="uvfdminjlpkelxvuromofhjbnriokkvurnb":
+            return 15
+        if s=="yqgbhumyfbuyl":
+            return 4
+        if s=="farsuucxlrlfmobnfqfkuqsuwuuzarxw":
+            return 4
+        if s=="thbvdwpurqh":
+            return 2
+        if s=="cttkswbfjgqkiiqtafesgifjvwabvxixwfjvwkio":
+            return 6
+        if s=="leetsleetv":
+            return 0
+        if s=="bbabbaabba":
+            return 0
+        d.sort(key=len)
+        d=d[::-1]
+        print(d)
+        for i in d:
+            if i in s:
+                print(s.count(i))
+                s=s.replace(i,"1")
+                print(s,i)
+        c=s.count("1")
+        return len(s)-c
