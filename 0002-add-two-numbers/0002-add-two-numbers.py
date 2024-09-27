fb8572fb13c7a1714bd5bb3666d33a53918cfaeb
Siddharth Kumar Jha <137885707+Sidhujha@users.noreply.github.com>
2024-09-27 12:22:24 +0530

    Time: 61 ms (20.12%), Space: 16.7 MB (30.5%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,26 +1,37 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class Node:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        s1=""
-        s2=""
-        while l1:
-            s1+=str(l1.val)
-            l1=l1.next
-        while l2:
-            s2+=str(l2.val)
-            l2=l2.next
-        s1=s1[::-1]
-        s2=s2[::-1]
-        s2=int(s1)+int(s2)
-        s2=str(s2)[::-1]
-        head=ListNode(s2[0])
+    def addTwoNumbers(self, head1: Optional[ListNode], head2: Optional[ListNode]) -> Optional[ListNode]:
+        head=Node(-1)
         cur=head
-        for i in s2[1:]:
-            cur.next=ListNode(i)
-            cur=cur.next
-        return head
-        
+        t1=head1
+        t2=head2
+        carry=0
+        while t1 or t2:
+            if t1!=None and t2!=None:
+                s=t1.val+t2.val+carry
+                carry=s//10
+                cur.next=Node(s%10)
+                cur=cur.next
+                t1=t1.next
+                t2=t2.next
+            
+            elif t1==None and t2!=None:
+                s=t2.val+carry
+                carry=s//10
+                cur.next=Node(s%10)
+                cur=cur.next
+                t2=t2.next
+            
+            elif t1!=None and t2==None:
+                s=t1.val+carry
+                carry=s//10
+                cur.next=Node(s%10)
+                cur=cur.next
+                t1=t1.next
+        if carry!=0:
+            cur.next=Node(carry)
+        return head.next
